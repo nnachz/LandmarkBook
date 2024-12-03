cbc187f5ed53fad27fdaba48ece5083d51f33f9b
Kerem Türközü <turkozukerem@gmail.com>
2024-12-04 00:10:40 +0300

    LandmarkBook added

---
+++ LandmarkBook/DetailsVC.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var landmarkLabel: UILabel!
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+        
+    }
+    
+
+   
+
+}
